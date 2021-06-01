240cfbefb4c79ec20a1f0c3823aadf3cd5d02a54
Mickael Mignard <mignard.mickael@gmail.com>
2021-06-01 14:53:38 +0200

    Mise au propre du code et des commentaires

--- js/script.js
+++ js/script.js
@@ -1,147 +1,181 @@
-/* =============== GESTION DE L'AFFICHAGE DES CATEGORIES DE VELOS (homme, femme, enfant) SUR LA PAGE INDEX ================================== */
+/* ==================== MANAGE DISPLAY OF CATEGORIES (vélo homme, femme, enfant) ON INDEX PAGE ========================= */
 
-// On récupère l'ensemble des liens déguisés en bouton (a) et on applique un écouteur d'évènement sur chacun.
-// Lorsque l'un d'eux est cliqué, on le passe en paramètre de la fonction displayCategory.
+// Get in the navbar all the links to the categories and put an event listener on each of them (links are disguised as buttons).
+// When a link is clicked, the link is passed as a parameter to displayCategory function.
 let categoryLinks = document.querySelectorAll('header .categories a');
 categoryLinks.forEach(a => {
     a.addEventListener('click', () => displayCategory(a));
 });
 
-
 /**
- * Fonction qui permet d'afficher la rubrique associée au bouton cliqué
+ * Function to display the category linked to the associated link. 
+ * When a link is clicked, the class 'displayed' is added to it and to the linked category in main section.
+ * The CSS properties attached to the class permit to display the category.
  * 
- * @param a  l'élément 'a' (lien déguisé en bouton du header) qui fait le lien vers une catégorie (Vélos Homme, Vélos Femme, Vélos Enfant)
+ * @param a the 'a' element (link disguised as a button) linked to a category
  * 
  */
 function displayCategory(a) {
-    // On récupère l'élément parent du lien qui a été cliqué
+    // Get the parent of the link clicked.   
     let li = a.parentNode;
 
-    // Si le lien était déjà sélectionné, pas de traitement. On sort de la fonction.
+    // If the link was already selected, no processing and exit of the function. 
     if (li.classList.contains('displayed')) {
-        return false
+        return false;
     }
 
-    // On retire la classe displayed du bouton qui était précédemment sélectionné
+    // IN THE NAVBAR:
+    // The class 'displayed' is removed from the link previously selected.   
     document.querySelector('.categories .displayed').classList.remove('displayed');
 
-    // On ajoute la classe displayed au bouton qui vient d'être cliqué
+    // The class 'displayed' is added to the clicked link. 
     li.classList.add('displayed');
 
-    // On retire la class displayed sur la rubrique qui était précédemment sélectionné
+    // IN THE MAIN SECTION:
+    // The class 'displayed' is removed from the category previously selected.   
     document.querySelector('.category.displayed').classList.remove('displayed');
 
-    // On ajoute la class displayed sur la rubrique correspondant au bouton cliqué
+    // The class 'displayed' is added to the category linked to the clicked link.
     document.querySelector(a.getAttribute('href')).classList.add('displayed');
-
 }
 
-/* =============================================== GESTION DU PANIER ===================================== */
+/* ================================================ MANAGE CART ============================================================ */
 
-// Data loaded from a JSON with AJAX
+// Data loaded from a JSON with AJAX.
 let products;
 let ourRequest = new XMLHttpRequest();
 ourRequest.open('GET', '../data.json');
 ourRequest.onload = () => {
+    // Parse the JSON file into JS objects and get an array of products.
     products = JSON.parse(ourRequest.responseText);
 };
 ourRequest.send();
 
-// Get the all the carts buttons to add to cart
+// Get all the cart's buttons to add articles in it.
 let addToCartButtons = document.querySelectorAll('.get-btn');
 
-// Add an event listener on each add to cart button
+// Get all buy now buttons to add to cart and display cart modal
+let buyNowButtons = document.querySelectorAll('.buy-btn');
+
+// Add an event listener on each button to add to cart.
 for (let i = 0; i < addToCartButtons.length; i++) {
     addToCartButtons[i].addEventListener('click', () => {
-        console.log("prod: " + products[i])
-        cartNumbers(products[i]);
-        totalCost(products[i]);
+        cartCounter(products[i]);
+        totalCost(products[i]);        
     })
 }
 
-// Check if cart contains products and display the numbers of items
-function onLoadCartNumbers() {
-    let productNumbers = localStorage.getItem('cartNumbers');
+// Add an event listener on each buy now buttons
+for (let i = 0; i < buyNowButtons.length; i++) {
+    buyNowButtons[i].addEventListener('click', () => {       
+        cartCounter(products[i]);
+        totalCost(products[i]);
+        displayCart();
+    })
+}
 
-    if (productNumbers) {
-        document.querySelector('.cart .items-counter').textContent = productNumbers;
+/**
+ * Check if cart contains products and display on index page the number of items.
+ */
+function onLoadCartCounter() {
+    let productsQuantity = localStorage.getItem('cartCounter');
+
+    if (productsQuantity) {
+        document.querySelector('.cart .items-counter').textContent = productsQuantity;
     }
 }
 
-// Count the number of items added to the cart
-function cartNumbers(product) {
-    let productNumbers = localStorage.getItem('cartNumbers');
+/**
+ * Increment by one the counter when a product is added in the cart and display it on index page.
+ * 
+ * @param product 
+ */
+function cartCounter(product) {
+    let productsQuantity = localStorage.getItem('cartCounter');
+    productsQuantity = parseInt(productsQuantity);
 
-    productNumbers = parseInt(productNumbers);
-
-    if (productNumbers) {
-        localStorage.setItem('cartNumbers', productNumbers + 1);
-        document.querySelector('.cart .items-counter').textContent = productNumbers + 1;
+    if (productsQuantity) {
+        localStorage.setItem('cartCounter', productsQuantity + 1);
+        document.querySelector('.cart .items-counter').textContent = productsQuantity + 1;
     } else {
-        localStorage.setItem('cartNumbers', 1);
+        localStorage.setItem('cartCounter', 1);
         document.querySelector('.cart .items-counter').textContent = 1;
     }
 
-    setItems(product);
+    addToJson(product);
 }
 
-// Add the different products in the cart (product), with their name (product.name) and number (product.inCart)
-function setItems(product) {
-    let cartItems = localStorage.getItem('productsInCart');
-    // Convert the JSON into a JavaScript object
-    cartItems = JSON.parse(cartItems);
-
-    // Check if if cart already contains items
-    if (cartItems != null) {
-        // Check with the name of the product if it is already in the cart
-        if (cartItems[product.id] == undefined) {
-            cartItems = {
-                ...cartItems,
+/**
+ * Add product in a JSON file stored in browser's local storage when added in cart.
+ * 
+ * @param product 
+ */
+function addToJson(product) {
+    // Get local storage JSON.
+    let productsInCart = localStorage.getItem('productsInCart');
+    // Convert the JSON into a JS object.
+    productsInCart = JSON.parse(productsInCart);  
+    // Check if JSON exists.
+    if (productsInCart != null) {       
+        // Check with the id of the product if it is already in the cart.           
+        if (productsInCart[product.id] == undefined) {
+            productsInCart = {
+                ...productsInCart,
                 [product.id]: product
-            }
-        }
-        // If there is at least one same type of product, add 1 to inCart
-        cartItems[product.id].inCart = parseInt(cartItems[product.id].inCart) + 1;
-        // If the cart is empty
-    } else {
+            };            
+        }          
+        // If there is at least one same type of product, add 1 to product's inCart value.
+        productsInCart[product.id].inCart = parseInt(productsInCart[product.id].inCart) + 1;
+                
+    // If the cart is empty.
+    } else {         
         product.inCart = 1;
-        cartItems = {
+        productsInCart = {
             [product.id]: product
         }
     }
-
-    // Convert into JSON
-    localStorage.setItem('productsInCart', JSON.stringify(cartItems));
+    // Convert into JSON.
+    localStorage.setItem('productsInCart', JSON.stringify(productsInCart));
 }
 
-// Compute the total price of the items in the cart
+/**
+ * Compute the total price of the items in the cart.
+ * 
+ * @param product 
+ */
 function totalCost(product) {
-    // Get the total price before to add a new item
+    // Get the total price before to add a new item.
     let cartCost = localStorage.getItem('totalCost');
 
-    if (cartCost != null) {
-        cartCost = parseInt(cartCost);
-        localStorage.setItem('totalCost', cartCost + parseInt(product.price));
-    } else {
-        localStorage.setItem('totalCost', parseInt(product.price));
+    if (cartCost != null) {        
+        cartCost = parseFloat(cartCost);
+        cartCost +=  parseFloat(product.price);
+        cartCost = cartCost.toFixed(2);
+        localStorage.setItem('totalCost', cartCost);
+    } else {        
+        localStorage.setItem('totalCost', product.price);
     }
 
 }
 
-// Add an event listener on the cart icon to display the content of the cart after a click
+// Add an event listener on the cart icon to display the content of the cart after a click.
 document.querySelector('.cart a').addEventListener('click', displayCart);
 
-// Display the content of the cart
+/**
+ * Display the content of the cart in a modal window.
+ */
 function displayCart() {
-    let cartItems = localStorage.getItem('productsInCart');
-    cartItems = JSON.parse(cartItems);
+    // Get JSON from local storage and parse it.
+    let productsInCart = localStorage.getItem('productsInCart');
+    productsInCart = JSON.parse(productsInCart);
+    // Get HTML element where to display the items added in the cart.
     let productContainer = document.querySelector('.products');
+    // Get total cost.
     let cartCost = localStorage.getItem('totalCost');
 
-    if (cartItems && productContainer) {
+    if (productsInCart && productContainer) {
         productContainer.innerHTML = '';
-        Object.values(cartItems).map(item => {
+        Object.values(productsInCart).map(item => {
             productContainer.innerHTML += `              
                 <tr class="text-center">
                     <td scope="row">${item.id}</th>
@@ -150,16 +184,15 @@ function displayCart() {
                     <td> ${parseInt(item.inCart)}</td>
                     <td><button class="cartAddButton btn btn-outline-info btn-sm"><i class="fas fa-plus"></i></button></td>
                     <td><button class="cartRemoveButton btn btn-outline-danger btn-sm"><i class="fas fa-minus"></i></button></td>
-                    <td>${parseInt(item.inCart) * parseInt(item.price)}</td>
+                    <td>${(parseInt(item.inCart) * parseFloat(item.price)).toFixed(2)}</td>
                 </tr>              
                 `;
         });
-
         productContainer.innerHTML += `
             <tfoot>
                 <tr class="fw-bold text-center">
                     <td colspan="4">TOTAL</td>
-                    <td colspan="3">${parseInt(cartCost)}€</td>
+                    <td colspan="3">${(parseFloat(cartCost)).toFixed(2)}€</td>
                 </tr>
             </tfoot>       
         `;
@@ -167,91 +200,106 @@ function displayCart() {
         productContainer.innerHTML = '';
     }
 
-    // // Get all add item buttons of the cart and add an event listener
+    // Get all add item buttons of the cart and add an event listener.
     let addItemButtons = document.querySelectorAll('table .cartAddButton');
     addItemButtons.forEach(button => button.addEventListener('click', addItem));
 
-    // // Get all remove item buttons of the cart and add an event listener
+    // Get all remove item buttons of the cart and add an event listener.
     let removeItemButtons = document.querySelectorAll('table .cartRemoveButton');
     removeItemButtons.forEach(button => button.addEventListener('click', removeItem));
 
 }
 
-
-// Add an item in the cart
+/**
+ * Add an item in the cart when plus sign button is hit in the cart's modal window.
+ * 
+ * @param e event
+ */
 function addItem(e) {
-    let id = e.currentTarget.parentElement.parentElement.firstElementChild.innerText;             
-    cartNumbers(products[id]);
+    // Get the product id in the cart's table.
+    let id = e.currentTarget.parentElement.parentElement.firstElementChild.innerText;
+    // Increment counter and total cost.
+    cartCounter(products[id]);
     totalCost(products[id]);
+    // Refresh cart.
     displayCart();
 }
 
-// Remove an item from the cart
+/**
+ * Remove an item from the cart when plus minus button is hit in the cart's modal window.
+ * 
+ * @param e event
+ */
 function removeItem(e) {
+    // Get the product id in the cart's table.
     let id = e.currentTarget.parentElement.parentElement.firstElementChild.innerText;
     let product = products[id];
-    let productNumbers = localStorage.getItem('cartNumbers');
+    // Update local storage variables and parse JSON file.
+    let productsQuantity = localStorage.getItem('cartCounter');
     let cartCost = localStorage.getItem('totalCost');
-    let cartItems = localStorage.getItem('productsInCart');
-    cartItems = JSON.parse(cartItems);
+    let productsInCart = localStorage.getItem('productsInCart');
+    productsInCart = JSON.parse(productsInCart);
 
-    // Remove item from cart if there is at leat one
-    if (productNumbers > 0 && cartItems[product.id].inCart > 0) {
-        // Remove one unit from cartNumbers in localStorage                  
-        localStorage.setItem('cartNumbers', productNumbers - 1);
-        // Remove one unit from the cart counter on the index webpage
-        document.querySelector('.cart .items-counter').textContent = productNumbers - 1;
-        // If there is at least one same type of product, remove one from inCart       
-        cartItems[product.id].inCart = parseInt(cartItems[product.id].inCart) - 1;
-        localStorage.setItem('productsInCart', JSON.stringify(cartItems));
+    // Remove item from cart if there is at least one.
+    if (productsQuantity > 0 && productsInCart[product.id].inCart > 0) {
+        // Remove one unit from cartCounter in localStorage.                  
+        localStorage.setItem('cartCounter', productsQuantity - 1);
+        // Remove one unit from the cart counter on the index page.
+        document.querySelector('.cart .items-counter').textContent = productsQuantity - 1;
+        // If there is at least one same type of product, remove one from inCart.       
+        productsInCart[product.id].inCart = parseInt(productsInCart[product.id].inCart) - 1;
+        if(productsInCart[product.id].inCart == 0){
+            delete productsInCart[product.id];
+        }    
+        // Convert into JSON.
+        localStorage.setItem('productsInCart', JSON.stringify(productsInCart));          
 
-        // Manage the total
-        if (cartCost != null) {
-            cartCost = parseInt(cartCost);
-            localStorage.setItem('totalCost', cartCost - parseInt(product.price));
-        } 
-
-        // Remove variable productsInCart from localStorage
-        if (productNumbers == 1) {
-            localStorage.removeItem('productsInCart');
+        // Manage the total.
+        if (cartCost != null) {            
+            cartCost = parseFloat(cartCost);
+            cartCost -=  parseFloat(product.price);
+            cartCost = cartCost.toFixed(2);
+            localStorage.setItem('totalCost', cartCost);
         }
 
+        // Remove variable productsInCart from localStorage.
+        if (productsQuantity == 1) {            
+          // localStorage.setItem('productsInCart', undefined); 
+          localStorage.clear();
+        }  
+        
     }
+    // Refresh cart.
     displayCart();
 }
 
+//***************** Button to clear out the cart ***********//
 
-onLoadCartNumbers();
-displayCart();
+// Get 'Vider le panier' button.
+document.getElementById('delete').addEventListener("click", resetCart);
 
-//*****************le button pour vider entièrement  le panier***********//
-
-
-   
-    document.getElementById('delete').addEventListener("click", resetCart);
-
-    function resetCart(){
-    
+/**
+ * Clear out the cart.
+ */
+function resetCart() {
     localStorage.removeItem("productsInCart");
     localStorage.removeItem("totalCost");
-    //localStorage.removeItem("cartNumbers");
-    
-    localStorage.setItem('cartNumbers',0);
-    document.querySelector('.cart .items-counter').textContent=0;
-    
-    
+    //localStorage.removeItem("cartCounter");
 
+    // Reset counter variable in local storage and its display.
+    localStorage.setItem('cartCounter', 0);
+    document.querySelector('.cart .items-counter').textContent = 0;
+    // Refresh cart.
     displayCart();
-    
-
-
 }
 
-//************************button acheter la commande *********************//
+//************************ Button 'passer la commande' *********************//
 
-document.getElementById('button-modal').addEventListener('click', function(){
-    document.getElementById('totalPrix').textContent= localStorage.getItem('totalCost');
+// Open modal window for payment when 'passer la commande' button is clicked.
+document.getElementById('button-modal').addEventListener('click', function () {
+    document.getElementById('totalPrix').textContent = localStorage.getItem('totalCost');
 })
 
- 
 
+// Get items in the cart count on page loading.
+onLoadCartCounter();
